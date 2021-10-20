5d22dbd37bb322a1b95e2511c82d2eb3173c44af
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-10-20 21:01:30 +0200

    Update script.js

--- JavaScript Programming for Beginners/04 - Conditional Statements/script.js
+++ JavaScript Programming for Beginners/04 - Conditional Statements/script.js
@@ -1,5 +1,11 @@
-let direction = '';
+let direction = 'left';
 
 if (direction == 'left') {
     console.log('red planet');
+} else if (direction == 'right') {
+    console.log('purple planet');
+} else if (direction == 'backwards') {
+    console.log('back home');
+} else {
+    console.log('crashed');
 }
