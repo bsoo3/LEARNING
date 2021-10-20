a29c1a5489c8c5b0591a67e8343d2a9be31949ab
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-10-20 20:50:09 +0200

    Update script.js

--- JavaScript Programming for Beginners/03 - Arithmetics Operators/script.js
+++ JavaScript Programming for Beginners/03 - Arithmetics Operators/script.js
@@ -13,3 +13,18 @@ b += 1;
 //substraction
 let x = 10 - 5;
 console.log(x);
+
+//multiplication
+let unitPrice = 2;
+let units = 10;
+let total = unitPrice * units;
+console.log(total);
+
+//divison
+let n = 10;
+let result = n / 2;
+console.log(result);
+
+//moduls
+let r = 5 % 2;
+console.log(r);
