cb4b6790750a55c9328797e615af96174759b42f
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-02 22:42:30 +0100

    Update script.js

--- JavaScript Programming for Beginners/08 - Objects/script.js
+++ JavaScript Programming for Beginners/08 - Objects/script.js
@@ -1,3 +1,28 @@
-let player = { age: 99 };
+let player = {
+    age: 99,
+    name: 'ABC',
+    isActive: true,
+    outfit: {
+        color: 'blue',
+        size: 'M',
+        cost: 100,
+        isNew: true
+    }
+};
 
 console.log(player);
+console.log(player.name);
+console.log(player['name']);
+
+player.isActive = false;
+console.log(player);
+
+player.health = 100;
+console.log(player);
+
+delete player.health;
+console.log(player);
+
+console.log(player.outfit.color);
+
+player.outfit.size = 'L';
