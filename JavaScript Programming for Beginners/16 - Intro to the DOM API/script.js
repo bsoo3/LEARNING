f204f89f08c96d9c948e697f731358cc766aa2bc
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-03 18:37:46 +0100

    Create script.js

---
+++ JavaScript Programming for Beginners/16 - Intro to the DOM API/script.js
@@ -0,0 +1,15 @@
+//1.select!!!!!!!!!
+let sign = document.getElementById('sign');
+console.log(sign);
+
+//2.modify
+console.log(sign.textContent);
+
+//modify text
+sign.textContent = 'Welcome, travelers!';
+
+//modify
+//sign.innerHTML = sign.innerHTML + '<p>hello</p>';
+
+//3. change style
+sign.style.color = ' blue';
