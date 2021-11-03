104676c6d5d622064696c6c1f16350e83bba8925
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-03 18:28:19 +0100

    Update script.js

--- JavaScript Programming for Beginners/15 - Multidimensional Arrays/script.js
+++ JavaScript Programming for Beginners/15 - Multidimensional Arrays/script.js
@@ -3,3 +3,17 @@ let a = [
     [4, 5, 6, 7]
 ];
 console.log(a);
+
+console.log(a[0][1]);
+
+a[1][2] = 100;
+
+console.log(a[1][2]);
+
+let terrains = [
+    ['desert', 'desert', 'grass', 'grass'],
+    ['desert', 'grass', 'water', 'grass'],
+    ['grass', 'grass', 'water', 'water'],
+    ['grass', 'grass', 'grass', 'grass']
+]
+console.log(terratins);
