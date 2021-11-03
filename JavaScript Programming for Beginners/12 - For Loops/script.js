5cf41fc85693b0938b0b7635f21c0a15f1f3da9b
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-03 18:00:11 +0100

    Create script.js

---
+++ JavaScript Programming for Beginners/12 - For Loops/script.js
@@ -0,0 +1,15 @@
+let population = 100;
+/*
+let i = 0;
+while (i < 10) {
+    pupulation *= 1.05;
+    i++;
+}
+console.log(population);
+*/
+
+for (let i = 0; i < 10; i++) {
+    population *= 1.05;
+
+}
+console.log(population);
