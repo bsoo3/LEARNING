4a1594265e9f56e1a79e3f462ca684f2bc377f9a
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-03 17:45:05 +0100

    Create script.js

---
+++ JavaScript Programming for Beginners/10 - While Loops/script.js
@@ -0,0 +1,18 @@
+function sendSignal() {
+    console.log('HELP');
+}
+
+let i = 0;
+
+while (i < 1000) {
+    sendSignal();
+    i++;
+}
+let result = 0;
+let j = 1;
+
+while (j <= 10) {
+    result += j;
+    j++;
+}
+console.log(reuslt);
