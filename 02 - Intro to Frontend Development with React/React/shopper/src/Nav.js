42569765407de6db0e26149b0ddd1a185e88de4e
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-06 14:21:27 +0100

    Update Nav.js

--- 02 - Intro to Frontend Development with React/React/shopper/src/Nav.js
+++ 02 - Intro to Frontend Development with React/React/shopper/src/Nav.js
@@ -1,29 +1,14 @@
 import React from "react";
 
-const Nav = ({activeTab, onTabChange}) => {
-
-    const itemClass = tabName =>
-    'App-nav-item' ${
-        activeTab === tabName ? 'selected' : ''
-    };
-
-    return(
-        <nav className="App-nav">
-            <ul>
-                <li className={itemClass('items')}>
-                    <button onClick={() => onTabChange('items')}>
-                        Items
-                    </button>
-                </li>
-                <li className = {itemClass('cart')}>
-                    <button onClick={() => onTabChange('cart')}>
-                        Cart
-                    </button>
-                </li>
-            </ul>
-        </nav>
-    );
-};
-
-
-export default Nav;
+const Nav = () => (
+    <nav className="App-nav">
+        <ul>
+            <li className="App-nav-item">
+                <button>Items</button>
+            </li>
+            <li className="App-nav-item">
+                <button>Cart</button>
+            </li>
+        </ul>
+    </nav>
+)
