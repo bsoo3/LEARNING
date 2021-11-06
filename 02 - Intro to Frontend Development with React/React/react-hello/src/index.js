1f45bab45f9d3aaea139123c3c51f0215250d317
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-06 13:49:36 +0100

    Update index.js

--- 02 - Intro to Frontend Development with React/React/react-hello/src/index.js
+++ 02 - Intro to Frontend Development with React/React/react-hello/src/index.js
@@ -2,19 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function HelloWorld(){
-  return <div><Hello/> <World/></div>
+  return <div>
+      <Hello/> 
+    </div>;
 }
 
 function Hello(){
+  /*
+  const name = 'Jane!';
   return(
-    <span>Hello</span>
+    <span>Hello {name} </span>
   );
+  */
+ var isHello = false;
+ return (
+   <span>{isHello ? 'Hello': 'Goodbye'}</span>
+ );
 }
-
+/*
 function World(){
   return <span>World</span>;
 }
-/*
+
 function Hello(){
   return React.createElement(
     'div',
