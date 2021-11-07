e855781f0b92360fe17779739fd71afc8c1aaf6c
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-07 12:41:50 +0100

    Update Item.js

--- 02 - Intro to Frontend Development with React/React/shopper/src/Item.js
+++ 02 - Intro to Frontend Development with React/React/shopper/src/Item.js
@@ -2,3 +2,32 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './Item.css';
 
+const Item = ({item, onAddToCart}) =>(
+    <div className="Item">
+        <div className="Item-left">
+            <div className="Item-image"/>
+            <div className="Item-title">
+                {item.name}
+            </div>
+            <div className="Item-description">
+                {item.descriptions}
+            </div>
+        </div>
+        <div className="Item-right">
+            <div className="Item-price">
+                ${item.price}
+            </div>
+            <button className="Item-addToCarrt">
+                Add to cart
+            </button>
+        </div>
+    </div>
+);
+
+Item.PropTypes = {
+    item: PropTypes.object.isRequired,
+    onAddToCart: PropTypes.func.isRequired
+};
+
+export default Item;
+
