327d081502235ad5c9e04c1127c384c88001720a
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-07 11:38:50 +0100

    Update static-data.js

--- 02 - Intro to Frontend Development with React/React/shopper/src/static-data.js
+++ 02 - Intro to Frontend Development with React/React/shopper/src/static-data.js
@@ -5,4 +5,13 @@ let items = [
         descriptions: "An iPad like no other.",
         price: 329.00
     },
+    {
+        id: 0,
+        name: "Apple iPad Pro",
+        descriptions: "Even more expensive then the iPad.",
+        price: 729.00
+    },
 ];
+
+
+export {items};
