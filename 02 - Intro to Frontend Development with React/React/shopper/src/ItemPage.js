3672a3204f1e233a6cb6b87e5ad14da6d4b549af
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-07 13:13:24 +0100

    Update ItemPage.js

--- 02 - Intro to Frontend Development with React/React/shopper/src/ItemPage.js
+++ 02 - Intro to Frontend Development with React/React/shopper/src/ItemPage.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
+import Item from './Item';
 import './ItemPage.css';
 
-function ItemPage({items}){
+function ItemPage({items, onAddToCart}) {
     return (
         <ul className="ItemPage-items">
             {items.map(item =>
                 <li key={item.id} className="ItemPage-item">
-                    {item.name}
-                </li>
+                    <Item item={item} onAddToCart={() => onAddToCart(item)}>
+                        <button className="Item-addToCart" 
+                        onClick={() => onAddToCart(item)}>
+                            Add to cart
+                        </button>
+                    </Item>
+                </li>    
             )}
 
         </ul>
     );
 }
 
-ItemPage.propTypes ={
+ItemPage.propTypes = {
     items: PropTypes.array.isRequired
 };
 
