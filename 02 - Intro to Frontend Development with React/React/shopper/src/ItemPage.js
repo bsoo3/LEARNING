cb84f0d67c63c0f3c95ee25d9048d145200f2f8c
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-07 11:33:16 +0100

    Create ItemPage.js

---
+++ 02 - Intro to Frontend Development with React/React/shopper/src/ItemPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import './ItemPage.css';
+
+function ItemPage({items}){
+    return (
+        <ul className="ItemPage-items">
+            {items.map(item =>
+                <li key={item.id} className="ItemPage-item">
+                    {item.name}
+                </li>
+            )}
+
+        </ul>
+    );
+}
