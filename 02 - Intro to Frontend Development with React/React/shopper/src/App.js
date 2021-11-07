d92863a341eb5eb6bc3595490fb15312d7e6af43
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-11-07 11:01:01 +0100

    Update App.js

--- 02 - Intro to Frontend Development with React/React/shopper/src/App.js
+++ 02 - Intro to Frontend Development with React/React/shopper/src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import Nav from './Nav';
 import './App.css';
 
-const App = () => {
 
+const App = () => {
+  const [activeTab, setActiveTab] = useState('items');
+
+  return(
+    <div className="App">
+        <Nav 
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+        />
+        <main className="App-content">
+            <Content tab={activeTab}/>
+        </main>
+    </div>
+  );
 };
 
+const Content = ({tab}) => {
+  switch(tab){
+    default:
+    case 'items':
+      return <span> the items</span>
+    case 'cart':
+      return <span> the cart</span>
 
+  }
+};
+
+export default App;
 
 /*
 function HelloWorld(){
